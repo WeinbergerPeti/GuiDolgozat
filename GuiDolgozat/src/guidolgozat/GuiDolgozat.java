bf73adfa1d2950c4c6f897ccdae3044b10eeb008
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-11-14 13:05:47 +0100

    Alap projekt indulás

---
+++ GuiDolgozat/src/guidolgozat/GuiDolgozat.java
@@ -0,0 +1,20 @@
+package guidolgozat;
+
+public class GuiDolgozat 
+{
+    public GuiDolgozat ()
+    {
+        ini();
+    }
+
+    public static void main(String[] args) 
+    {
+        new GuiDolgozat();
+    }
+    
+    private void ini()
+    {
+        
+    }
+    
+}
